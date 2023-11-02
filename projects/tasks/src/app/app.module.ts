192fde6fcd2312cc3116ea4f5bb802ab485a1697
Oliver <201806928@post.au.dk>
2023-11-02 12:20:21 +0100

    main app

--- projects/tasks/src/app/app.module.ts
+++ projects/tasks/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskoverviewComponent } from './taskoverview/taskoverview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskoverviewComponent
   ],
   imports: [
     BrowserModule,
