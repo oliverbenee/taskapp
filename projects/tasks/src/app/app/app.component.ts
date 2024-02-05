28f42b475e208485b0a662221aa56d6412febcbd
Oliver <201806928@post.au.dk>
2024-02-05 20:08:42 +0100

    Update app.component.ts

--- projects/tasks/src/app/app/app.component.ts
+++ projects/tasks/src/app/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { TaskService } from '../service/task.service';
 import { Task } from '../model/tasks'
 
+/**
+ * This is a smart component. Because it has to receive emissions from the task list to add to the service. 
+ */
 @Component({
   selector: 'tasks-root',
   templateUrl: './app.component.html'
